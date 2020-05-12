e3ac82be30e7851a3d045e042ce4bc0d2517ee46
federico barreca <barreca.1736423@studenti.uniroma1.it>
2020-05-12 18:22:16 +0200

    Task 1 ex9

--- 9_DGraphs/java-aux/GraphServices.java
+++ 9_DGraphs/java-aux/GraphServices.java
@@ -1,14 +1,43 @@
+import java.util.*;
 public class GraphServices<V>{
 
-  public static <V> void sweep(Graph<V> g) {
-	  
-  }
+	private static void DFSsweep(Graph.GraphNode gn, int timestamp){
+		List<Graph.GraphNode> out_nodes = gn.outEdges;
+		gn.state = Graph.GraphNode.Status.EXPLORING;
+		gn.timestamp = timestamp; 
+		for(Graph.GraphNode gnNext : out_nodes){
+			if(gnNext.state == Graph.GraphNode.Status.UNEXPLORED){
+				System.out.println(gn.value + " -> " + gnNext.value + " : TREE");
+				DFSsweep(gnNext, timestamp+1);
+			}
+			else if(gnNext.state == Graph.GraphNode.Status.EXPLORING){
+				System.out.println(gn.value + " -> " + gnNext.value + " : BACK");
+			}
+			else if(gnNext.state == Graph.GraphNode.Status.EXPLORED && gnNext.timestamp>timestamp){
+				System.out.println(gn.value + " -> " + gnNext.value + " : FORWARD");
+			}
+			else{
+				System.out.println(gn.value + " -> " + gnNext.value + " : CROSS");
+			}
+			
+		}
+		gn.state = Graph.GraphNode.Status.EXPLORED;
+	}
+
+	public static <V> void sweep(Graph<V> g) {
+		List<Graph.GraphNode<V>> nodes = g.getNodes();
+		for(Graph.GraphNode<V> gn : nodes){
+			if(gn.state == Graph.GraphNode.Status.UNEXPLORED){
+				DFSsweep(gn, 1);	
+			}
+		}
+	}
   
-  public static <V> void topologicalSort(Graph<V> g) {
+	public static <V> void topologicalSort(Graph<V> g) {
 	  
-  }
+	}
   
-  public static <V> void strongConnectedComponents(Graph<V> g) {
+	public static <V> void strongConnectedComponents(Graph<V> g) {
 	  
-  }
+	}
 }
